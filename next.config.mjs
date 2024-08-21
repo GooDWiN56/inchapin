db1fcefd053b195786e3a75766e34d59e10e60ca
Goodwin <d.a.kozhyshko@gmail.com>
2024-08-21 05:05:40 +0300

    change

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,14 @@
 /** @type {import('next').NextConfig} */
 import path from "path";
 
+const isProd = (process.env.NODE_ENV = "production");
+
 const sassOptions = {
   includePaths: [path.join(process.cwd(), "styles")],
 };
 
 const nextConfig = {
+  basePath: isProd ? "/inchapin" : "",
   output: "export",
   distDir: "dist",
   images: {
